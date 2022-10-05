db1dd8dbd9593b53b0dea722e34406e6076ecad1
Elisangela2205 <112012190+Elisangela2205@users.noreply.github.com>
2022-10-05 16:47:40 -0300

    Add files via upload

---
+++ aula2php/ex1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>programação php</h1>
+   <?php
+   //comentario de linha
+
+   /*comentario de bloco*/
+   echo"<h1>programação php</h1>";
+   //declareação de variaveis 
+   $nome= "Elisangela";//Tipo texto
+   $idade= 25;//Inteiro
+
+   //processamento
+
+   //Saida na tela de impressão
+   echo"<br/> Nome: ".$nome;
+   echo"<br/> Idade: ".$idade;
+
+   ?>
+</body>
+</html>
